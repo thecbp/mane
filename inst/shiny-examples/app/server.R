4e8e3e6c30fb364a828543f8855cdde2c113b02f
Christian Pascual <chris.b.pascual@gmail.com>
2023-02-06 11:30:28 -0800

    Added progress bar for simulation

--- inst/shiny-examples/app/server.R
+++ inst/shiny-examples/app/server.R
@@ -1,16 +1,46 @@
+source("helpers")
+
 server <- function(input, output, session) {
+
+  simulation_data = eventReactive(input[["simulate"]], {
+
+    treatment_effect_input_names = paste0("trt-effect-", 1:input[["n-trts"]])
+    betas = input[treatment_effect_input_names] %>% unlist()
+
+    withProgress(message = "Simulating Platform-of-1 trials", {
+      for (i in seq_len(input[["n-sims"]])) {
+
+        sim = simulate(n_trts = input[["n-trts"]],
+                       n_burn_cycles = input[["n-burn-cycles"]],
+                       n_adaptive_periods = input[[]],
+                       betas = betas,
+                       y_sigma = input[["within-person-noise"]],
+                       n_chains = input[["n-chains"]],
+                       n_iter = input[["samples-per-chain"]],
+                       lag = 1, # RECHECK LATER
+                       stabilize = NULL,
+                       objective = input[["objective"]],
+                       adapt_delta = input[["adapt-delta"]],
+                       max_treedepth = input[["max-treedepth"]])
+
+        # Increment progress on the progress bar
+        incProgress(1 / input$steps)
+      }
+      # ADD LATER: SOME STRUCTURE THAT PUTS TOGETHER ALL OF THE SIM DATA
+    })
+
+
+
+  })
 
   # Create UI to specify the true treatment effects
   output$modelControls = renderUI({
 
     model_parameter_panel = purrr::map(1:input[["n-trts"]], function(trt) {
 
-      fluidRow(
-        column(12, numericInput(inputId = paste0("trt-", trt, "-mean"),
+      fluidRow(column(12, numericInput(inputId = paste0("trt-effect-", trt),
                                label = paste0("Treatment ", trt, " Effect"),
-                               value = 0))
-
-      )
+                               value = 0)))
 
     })
 
@@ -45,8 +75,13 @@ server <- function(input, output, session) {
   })
 
   output$test = renderText({
-    input[["burn-in-lengths"]]
+
+
+
+
   })
+
+
 
 
 
