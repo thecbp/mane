9850b02603cb49653edf3a64499bb32b6939b013
Christian Pascual <chris.b.pascual@gmail.com>
2023-02-06 19:17:05 -0800

    Adding individual level trial simulation function

---
+++ R/simulate.R
@@ -0,0 +1,16 @@
+simulate = function(n_trts, n_burn_cycles, n_adaptive_periods,
+                    betas, y_sigma, n_chains, n_iter, lag,
+                    stabilize = NULL,
+                    objective = "maximize",
+                    adapt_delta = 0.999,
+                    max_treedepth = 15,
+                    seed = 1) {
+
+  set.seed(seed)
+
+  # Stopping the RMD CHECK for global variables
+  id = NULL
+
+
+
+}
