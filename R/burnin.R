6cdd03cfe7bc8c15d5a796b0f235406a622fcc86
Christian Pascual <chris.b.pascual@gmail.com>
2023-04-04 17:31:36 -0700

    Fixed burnin function

--- R/burnin.R
+++ R/burnin.R
@@ -14,7 +14,6 @@ burnin = function(n_trts, n_burn_cycles, burn_obvs_per_period, betas, y_sigma, p
   # Matrix of single person getting all of the treatments for n_obvs each
   X = diag(n_trts)
   X[, 1] = 1
-  colnames(X) = paste0("X", 1:n_trts)
   X = X[rep(seq_len(nrow(X)), each = burn_obvs_per_period),]
   X = X[rep(seq_len(nrow(X)), times = n_burn_cycles),]
 
@@ -32,6 +31,8 @@ burnin = function(n_trts, n_burn_cycles, burn_obvs_per_period, betas, y_sigma, p
     dplyr::mutate(
       period = rep(1:n_trts, times = n_burn_cycles * burn_obvs_per_period)
     )
+
+  colnames(output) = c(paste0("X", 1:n_trts), "Y", "period")
 
   output
 
